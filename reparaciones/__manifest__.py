eb2c2b9666196cc8ee898e368de43b23fadaa737
fcoabel <58517727+fcoabel@users.noreply.github.com>
2021-02-25 07:40:26 +0100

    Add files via upload

---
+++ reparaciones/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'reparacion',
+    'description': 'Gestion de reparaciones de un taller',
+    'author': 'Fco Abel Pelegrín Mañas',
+    'depends': ['mail'],
+    'application': True,
+    'data:' ['views/reparacion.xml']
+}
