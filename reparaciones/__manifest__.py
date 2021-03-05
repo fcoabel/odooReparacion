d2ad237367da35f0bff0a06742cf53c0c16c3587
fcoabel <58517727+fcoabel@users.noreply.github.com>
2021-03-05 06:03:08 +0100

    Delete reparaciones directory

--- reparaciones/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'reparacion',
-    'description': 'Gestion de reparaciones de un taller',
-    'author': 'Fco Abel Pelegrín Mañas',
-    'depends': ['mail'],
-    'application': True,
-    'data:' ['views/reparacion.xml']
-}
